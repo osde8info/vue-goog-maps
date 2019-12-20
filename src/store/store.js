5cdff934b79e3186a2657ec9b4928a3f5412bb15
Super Diana <25756676+alphacentauri82@users.noreply.github.com>
2019-12-20 10:51:52 -0500

    Adding Nexmo

--- src/store/store.js
+++ src/store/store.js
@@ -57,6 +57,65 @@ export default new Vuex.Store({
         dispatch('notification/add', notification, { root: true })
       }
     },
+    import Vue from 'vue'
+import Vuex from 'vuex'
+import firebase from '@/utils/firebase'
+
+// importing modules
+import * as notification from '@/store/modules/notification.js'
+import * as user from '@/store/modules/user.js'
+import * as adminUsers from '@/store/modules/adminUsers.js'
+
+Vue.use(Vuex)
+
+/**
+ *
+ * @param {*} type Type of the Notification
+ * @param {*} message Message to display as Notification
+ * @param {*} dispatch Dispatch object of Vuex
+ */
+const publishNotification = (type, message, dispatch) => {
+  const notification = { type, message }
+  dispatch('notification/add', notification, { root: true })
+}
+
+export default new Vuex.Store({
+  modules: {
+    user,
+    notification,
+    adminUsers
+  },
+  state: {
+    coordinates: {
+      lat: null,
+      lng: null
+    },
+    positionAccuracy: 0
+  },
+  mutations: {
+    SET_LOCATION(state, coor) {
+      // setting the coordinates of the position
+      state.coordinates = { lat: coor.latitude, lng: coor.longitude }
+      // setting the accuracy of the position
+      state.positionAccuracy = coor.accuracy
+    }
+  },
+  actions: {
+    getCurrentLocation({ commit, dispatch }) {
+      if (navigator.geolocation) {
+        // gettingt the position using the HTML5 API
+        navigator.geolocation.getCurrentPosition(position => {
+          commit('SET_LOCATION', position.coords)
+        })
+      } else {
+        // display notification of lack of support
+        const notification = {
+          type: 'warning',
+          message: 'Geolocation is not supported by this browser.'
+        }
+        dispatch('notification/add', notification, { root: true })
+      }
+    },
     sendPanicSMS({ state, dispatch }) {
       // accesing the Cloud Function
       const sendSMS = firebase.functions().httpsCallable('sendSMS')
@@ -107,3 +166,4 @@ export default new Vuex.Store({
     }
   }
 })
+
